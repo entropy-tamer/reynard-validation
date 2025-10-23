430e8516a907b3b9457f110153f9e7d7ec617296
Balazs Horvath <acsipont@gmail.com>
2025-10-23 12:28:46 +0200

    chore: sync package from monorepo v0.44.22

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -3,7 +3,7 @@
  * Uses unified core test setup from reynard-testing
  */
 
-import { setupCoreTest } from "reynard-testing";
+import { setupCoreTest } from "@entropy-tamer/reynard-testing";
 
 // Use unified core test setup
 setupCoreTest();
